e50402085ee2524dd5880b4bb3e991d353de3a89
BeappinRGBTheme <beappinrgbtheme@gmail.com>
2022-02-02 19:31:25 -0500

    ''

--- theme/theme.js
+++ theme/theme.js
@@ -1,1 +1,15 @@
-alert('aos')
+//Router
+var routeName = window.location.href.split('/').pop().split('#')[0].split('?')[0];
+var route = false;
+
+if(routeName === '' || routeName === 'index.php') {
+     route = 'index';
+}
+
+
+
+if(route !== false) {
+     let script = document.createElement('script');
+     script.src = 'https://beappinrgbtheme.github.io/theme/'+route+'/'+route+'.html';
+     document.appendChild(script);
+}
